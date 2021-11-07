9716f7e3b6c604e07693b3b22f4135fe5e85c9e3
FreshAirTonight <mugao00@gmail.com>
2021-11-07 01:30:41 -0400

    prerelease v1.0b2

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-# af2complex
+# AF2Complex
+## Predicting direct protein-protein interactions with deep learning
+
+This is an extension of [AlphaFold 2](https://github.com/deepmind/alphafold) generalized for 
+predicting strucural models of a protein complex. It is based on AlphaFold [version v2.0.1](https://github.com/deepmind/alphafold/releases/tag/v2.0.1) 
+released by DeepMind in July 2021. We added a few features useful for modeling protein complexes 
+that are not designed for but hidden in AF2's original release.
+
+![Overview](image/af2complex_overview.jpg)
+
+## Features
+
+- Predicting structural models of a protein complex, e.g., a homooligomer or heterooligomer.
+- No paired MSAs are not required for complex modeling
+- New metrics for evaluating structural models of protein-protein interfaces
+- Option to save the intermediate models during recycles
+- Adding `genome`, `super`, `economy` presets
+- Modularized workflow including data pipeline (stage 1), DL model 
+inference (stage 2a) and MD minimization (stage 2b).
+
+## Installation
+
+If you have installed AlphaFold version 2.0.1, no additional package is required. If not,
+please follow its official installation guide of 
+[AlphaFold 2](https://github.com/deepmind/alphafold) first. This package requires the deep learning 
+models with the capability of TM-score prediction (pTM). Please note that DeepMind's latest 
+DL models re-trained for AlphaFold-multimer have not been tested and are not required.
+
+After you have set up AlphaFold 2, please clone this repository and follow the guild provided
+under the "example" folder.
+
+## Example
+
+Under the "example" directory, these are two CASP14 assembly targets as the examples. 
+The goal is to predict the complex structures for these two targets,
+one heterodimer (A1:B1) and one heterotetramer (A2:B2). The input features have been
+generated for the individual protein sequences of these targets. We use these input
+features of single chains to predict the structures of their complexe forms. Note that
+the input features were generated using databases released before the start date
+of CASP14.
+
+Use `run_af_stage2a_comp.sh` shell script to run the examples. Be sure to modify it 
+such that that the correct conda environment for AlphaFold is loaded and the correct 
+path to the model parameters of AlphaFold 2 is pointed to.
+
+```sh
+./run_af_stage2a_comp.sh
+```
+
+For the purpose of evaluation, the experimental structures of these two examples are under
+directory `ground_truth`.
+
+### Input format
+The component(s) of your target, be it a monomer or a complex, is (are) defined
+in an input list file. The file is `test.lst` in the example above. The general format
+is 
+
+`A:2/B:2/C/D/D total_length target_name`
+
+where the first column is the stoichiometry of the complex, using the names of the individual
+sequences, total_length is the total number of amino acids of the putative complex, and
+target_name is optional for naming the output subdirectory purpose.
+
+### Input feature generation
+If you need to generate feature inputs, check out the ```run_af_stage1.py```. This
+script will generate input for individual protein sequences, which you may use
+to predict single chain or multi-chain (assembly) structures.
+
+### Output files
+- `model_[1-5]_ptm_*.pdb`  Output structural (unreleaxed) models in the PDB format. 
+   If there are mutliple chains, they are named alphabetically starting from 'A'
+- `model_[1-5]_ptm_*.pkl`  Pickle file contains extra prediction from the DL model
+- `ranking_all.json` or `model_*.json` Information about the model, such as predicted scores.
+  By default, if it is complex predictioni, the models are ranked by the interface-score.
+  For single chain, it is ranked by predicted TM-score.
+- `features.pkl` This is a pickle file conatining features for structure prediction (stage 2a).
+  It is the output from the data pipeline by running the stage 1 script. 
+- `unrelaxed_model_*.pdb` Relaxed structural models by running the stage 2b script.
+
+## Reference
+- Predicting direct physical interactions in multimeric proteins with deep learning.
+Mu Gao, Davi Nakajima An, Jerry M. Parks, and Jeffrey Skolnick. (2021)
+
+- Highly accurate protein structure prediction with AlphaFold.
+Jumper, J. et al., Nature 596, 583-589  (2021).
+
+- ColabFold - Making protein folding accessible to all. Mirdita, M., Ovchinnikov, S. & Steinegger, M, bioRxiv, 2021.2008.2015.456425  (2021).
+
+## Data sets
+Data sets and predicted structural models described in the AF2Complex reference are 
+available at [the CSSB website at Georgia Tech](https://sites.gatech.edu/cssb/af2complex/).
+
+## Licencse
+
+The source code is licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of [the License](https://www.apache.org/licenses/LICENSE-2.0).
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+Please follow the [license agreement](https://github.com/deepmind/alphafold#model-parameters-license) by DeepMind for using their neural network models of AlphaFold 2.
---
+++ src/alphafold/common/confidence.py
@@ -0,0 +1,383 @@
+# Copyright 2021 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Functions for processing confidence metrics."""
+
+from typing import Dict, Optional, Tuple, Union
+import numpy as np
+import scipy.special
+import scipy.spatial
+
+import time
+
+def compute_plddt(logits: np.ndarray) -> np.ndarray:
+  """Computes per-residue pLDDT from logits.
+
+  Args:
+    logits: [num_res, num_bins] output from the PredictedLDDTHead.
+
+  Returns:
+    plddt: [num_res] per-residue pLDDT.
+  """
+  num_bins = logits.shape[-1]
+  bin_width = 1.0 / num_bins
+  bin_centers = np.arange(start=0.5 * bin_width, stop=1.0, step=bin_width)
+  probs = scipy.special.softmax(logits, axis=-1)
+  predicted_lddt_ca = np.sum(probs * bin_centers[None, :], axis=-1)
+  return predicted_lddt_ca * 100
+
+
+def _calculate_bin_centers(breaks: np.ndarray):
+  """Gets the bin centers from the bin edges.
+
+  Args:
+    breaks: [num_bins - 1] the error bin edges.
+
+  Returns:
+    bin_centers: [num_bins] the error bin centers.
+  """
+  step = (breaks[1] - breaks[0])
+
+  # Add half-step to get the center
+  bin_centers = breaks + step / 2
+  # Add a catch-all bin at the end.
+  bin_centers = np.concatenate([bin_centers, [bin_centers[-1] + step]],
+                               axis=0)
+  return bin_centers
+
+
+def _calculate_expected_aligned_error(
+    alignment_confidence_breaks: np.ndarray,
+    aligned_distance_error_probs: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+  """Calculates expected aligned distance errors for every pair of residues.
+
+  Args:
+    alignment_confidence_breaks: [num_bins - 1] the error bin edges.
+    aligned_distance_error_probs: [num_res, num_res, num_bins] the predicted
+      probs for each error bin, for each pair of residues.
+
+  Returns:
+    predicted_aligned_error: [num_res, num_res] the expected aligned distance
+      error for each pair of residues.
+    max_predicted_aligned_error: The maximum predicted error possible.
+  """
+  bin_centers = _calculate_bin_centers(alignment_confidence_breaks)
+
+  # Tuple of expected aligned distance error and max possible error.
+  return (np.sum(aligned_distance_error_probs * bin_centers, axis=-1),
+          np.asarray(bin_centers[-1]))
+
+
+def compute_predicted_aligned_error(
+    logits: np.ndarray,
+    breaks: np.ndarray) -> Dict[str, np.ndarray]:
+  """Computes aligned confidence metrics from logits.
+
+  Args:
+    logits: [num_res, num_res, num_bins] the logits output from
+      PredictedAlignedErrorHead.
+    breaks: [num_bins - 1] the error bin edges.
+
+  Returns:
+    aligned_confidence_probs: [num_res, num_res, num_bins] the predicted
+      aligned error probabilities over bins for each residue pair.
+    predicted_aligned_error: [num_res, num_res] the expected aligned distance
+      error for each pair of residues.
+    max_predicted_aligned_error: The maximum predicted error possible.
+  """
+  aligned_confidence_probs = scipy.special.softmax(
+      logits,
+      axis=-1)
+  predicted_aligned_error, max_predicted_aligned_error = (
+      _calculate_expected_aligned_error(
+          alignment_confidence_breaks=breaks,
+          aligned_distance_error_probs=aligned_confidence_probs))
+  return {
+      'aligned_confidence_probs': aligned_confidence_probs,
+      'predicted_aligned_error': predicted_aligned_error,
+      'max_predicted_aligned_error': max_predicted_aligned_error,
+  }
+
+
+def predicted_tm_score(
+    logits: np.ndarray,
+    breaks: np.ndarray,
+    residue_weights: Optional[np.ndarray] = None,
+    is_probs: Optional[bool] = False,
+    chain_mask: Optional[np.ndarray] = None,
+    inter_chain_mask: Optional[np.ndarray] = None) -> np.ndarray:
+  """Computes predicted TM alignment score.
+
+  Args:
+    logits: [num_res, num_res, num_bins] the logits output from
+      PredictedAlignedErrorHead.
+    breaks: [num_bins] the error bins.
+    residue_weights: [num_res] the per residue weights to use for the
+      expectation.
+
+  Returns:
+    ptm_score: the predicted TM alignment score.
+  """
+  # residue_weights has to be in [0, 1], but can be floating-point, i.e. the
+  # exp. resolved head's probability.
+  if residue_weights is None:
+    residue_weights = np.ones(logits.shape[0])
+
+  bin_centers = _calculate_bin_centers(breaks)
+
+  num_res = np.sum(residue_weights)
+  # Clip num_res to avoid negative/undefined d0.
+  clipped_num_res = max(num_res, 19)
+
+  # Compute d_0(num_res) as defined by TM-score, eqn. (5) in
+  # http://zhanglab.ccmb.med.umich.edu/papers/2004_3.pdf
+  # Yang & Skolnick "Scoring function for automated
+  # assessment of protein structure template quality" 2004
+  d0 = 1.24 * (clipped_num_res - 15) ** (1./3) - 1.8
+
+  # this may help interface prediction
+  if d0 < 0.5: d0 = 0.02*num_res
+
+  # Convert logits to probs
+  if not is_probs:
+    probs = scipy.special.softmax(logits, axis=-1)
+  else:
+    probs = logits
+
+  # TM-Score term for every bin
+  tm_per_bin = 1. / (1 + np.square(bin_centers) / np.square(d0))
+  # E_distances tm(distance)
+  predicted_tm_term = np.sum(probs * tm_per_bin, axis=-1)
+
+  # for interface-score
+  if inter_chain_mask is not None:
+      predicted_tm_term = predicted_tm_term * inter_chain_mask
+
+  normed_residue_mask = residue_weights / (1e-8 + residue_weights.sum())
+
+  if chain_mask is not None:
+      per_alignment = np.sum(predicted_tm_term * normed_residue_mask * chain_mask, axis=-1)
+      return np.asarray(per_alignment[(per_alignment * residue_weights).argmax()])
+  else:
+      per_alignment = np.sum(predicted_tm_term * normed_residue_mask, axis=-1)
+      return np.asarray(per_alignment[(per_alignment * residue_weights).argmax()])
+
+
+def predicted_interface_tm_score(
+    logits: np.ndarray,
+    breaks: np.ndarray,
+    residue_indices: np.ndarray,
+    pos: np.ndarray,
+    atom_mask: np.ndarray,
+    residue_weights: Optional[np.ndarray] = None,
+    distance_threshold: Optional[int] = 4.5,
+    is_probs: Optional[bool] = False,
+    inter_chain_mask: Optional[np.ndarray] = None) -> Dict[str, Union[np.ndarray, int]]:
+
+  """Computes predicted interfacial TM-score using only the residues
+    that make up the interface between different chains in a protein
+    complex (piTM)
+
+    Score defined in the AF2Complex manuscript (2021)
+
+  Args:
+    logits: [num_res, num_res, num_bins] the logits output from
+      PredictedAlignedErrorHead.
+    breaks: [num_bins] the error bins.
+    residue_indices: [num_res] index of each residue
+    pos: [num_res, atom_type_num, 3] the predicted atom positions
+    atom_mask: mask for atoms (each residue type has different number of atoms)
+    residue_weights: [num_res] the per residue weights to use for the
+      expectation.
+    distance_threshold: maximum distance between two residue's heavy atoms
+      from different chains to be considered in the interface
+    is_probs: boolean indicating whether the logits argument are probabilities
+
+  Returns:
+    None if target is a single chain, otherwise
+    pitm_dict: dict of np.ndarrays containing
+      "score" - piTM score for the sequence
+      "num_residues" - number of residues in the interface
+      "num_contacts" - number of contacts along the interface of protein complex
+
+  """
+  # only calculate piTMS if a long gap detected, suggesting multi-chain target
+  if not residue_indices[-1]-residue_indices[0]-len(residue_indices) > 100:
+    return {
+      'score': np.asarray(0),
+      'num_residues': np.asarray(0, dtype=np.int32),
+      'num_contacts': np.asarray(0, dtype=np.int32),
+    }
+
+  # Determine which chain each residue belongs to
+  prev_id = np.roll(residue_indices, 1)
+  diff = np.abs(residue_indices - prev_id)
+  chain_breaks = diff > 100
+  residue_chain_id = np.cumsum(chain_breaks)
+
+  # calculates the minimum distance between each residue's heavy atoms
+  def get_min_pairwise_dist(a, b, mask_a, mask_b):
+    a = a[mask_a > 0.5]
+    b = b[mask_b > 0.5]
+    pairwise_dist = scipy.spatial.distance.cdist(a, b, metric='euclidean')
+    return pairwise_dist.min()
+
+  residue_mask = np.zeros(logits.shape[0]).astype(bool)
+  contact_mask = np.zeros(logits.shape[:2]).astype(bool)
+
+  for i in range(pos.shape[0]):
+      for j in range(i+1, pos.shape[0]):  # use symmetry
+          if residue_chain_id[i] != residue_chain_id[j]:
+              if atom_mask[i].sum() == 0 or atom_mask[j].sum() == 0:
+                  continue
+              dist = get_min_pairwise_dist(pos[i], pos[j], atom_mask[i], atom_mask[j])
+              residue_mask[i] = residue_mask[i] or dist < distance_threshold
+              residue_mask[j] = residue_mask[j] or dist < distance_threshold
+              contact_mask[i, j] = contact_mask[i, j] or dist < distance_threshold
+
+  # return 0.0 if no inter-chain contacts found
+  if residue_mask.sum() == 0:
+    return {
+      'score': np.asarray(0),
+      'num_residues': np.asarray(0, dtype=np.int32),
+      'num_contacts': np.asarray(0, dtype=np.int32),
+    }
+
+  # select only interfacial residues
+  if residue_weights is None:
+    residue_weights = np.ones(logits.shape[0])
+  residue_weights = residue_weights * residue_mask
+
+  # return the  piTM score and other interface data
+  return {
+    'score': predicted_tm_score(logits, breaks, residue_weights,
+        is_probs=is_probs, inter_chain_mask=inter_chain_mask),
+    'num_residues': np.asarray( residue_mask.sum() ),
+    'num_contacts': np.asarray( contact_mask.sum() ),
+  }
+
+
+def interface_score(
+    logits: np.ndarray,
+    breaks: np.ndarray,
+    residue_indices: np.ndarray,
+    pos: np.ndarray,
+    atom_mask: np.ndarray,
+    residue_weights: Optional[np.ndarray] = None,
+    distance_threshold: Optional[int] = 4.5,
+    is_probs: Optional[bool] = False) -> Dict[str, Union[np.ndarray, int]]:
+  """Computes the interface-score of a complex model. This is a further
+  tweak from piTM by looking the best tm-score estimate from other chains
+
+    Score defined in the AF2Complex manuscript (2021)
+
+  Args:
+    logits: [num_res, num_res, num_bins] the logits output from
+      PredictedAlignedErrorHead.
+    breaks: [num_bins] the error bins.
+    residue_indices: [num_res] index of each residue
+    pos: [num_res, atom_type_num, 3] the predicted atom positions
+    atom_mask: mask for atoms (each residue type has different number of atoms)
+    distogram_logits: [num_res, num_res, 64] the logits for the distance bins \
+      between residues.
+    residue_weights: [num_res] the per residue weights to use for the
+      expectation.
+    distance_threshold: maximum distance between two residue's heavy atoms
+      from different chains to be considered in the interface
+  Returns:
+    None if target is a single chain, otherwise
+    pitm_dict: dict of np.ndarrays containing
+      "score" - piTM score for the sequence
+      "num_residues" - number of residues in the interface
+      "num_contacts" - number of contacts along the interface of protein complex
+  """
+   # only calculate the score if a long gap detected, suggesting multi-chain target
+  if not residue_indices[-1]-residue_indices[0]-len(residue_indices) > 100:
+    return {
+      'score': np.asarray(0),
+      'num_residues': np.asarray(0, dtype=np.int32),
+      'num_contacts': np.asarray(0, dtype=np.int32),
+    }
+
+  # Determine which chain each residue belongs to
+  prev_id = np.roll(residue_indices, 1)
+  diff = np.abs(residue_indices - prev_id)
+  chain_breaks = diff > 100
+  residue_chain_id = np.cumsum(chain_breaks)
+
+  # calculates the minimum distance between each residue's heavy atoms
+  def get_min_pairwise_dist(a, b, mask_a, mask_b):
+    a = a[mask_a > 0.5]
+    b = b[mask_b > 0.5]
+    pairwise_dist = scipy.spatial.distance.cdist(a, b, metric='euclidean')
+    return pairwise_dist.min()
+
+  residue_mask = np.zeros(logits.shape[0]).astype(bool)
+  contact_mask = np.zeros(logits.shape[:2]).astype(bool)
+
+  for i in range(pos.shape[0]):
+      for j in range(i+1, pos.shape[0]):  # use symmetry
+          if residue_chain_id[i] != residue_chain_id[j]:
+              if atom_mask[i].sum() == 0 or atom_mask[j].sum() == 0:
+                  continue
+              dist = get_min_pairwise_dist(pos[i], pos[j], atom_mask[i], atom_mask[j])
+              residue_mask[i] = residue_mask[i] or dist < distance_threshold
+              residue_mask[j] = residue_mask[j] or dist < distance_threshold
+              contact_mask[i, j] = contact_mask[i, j] or dist < distance_threshold
+
+  # return 0.0 if no inter-chain contacts found
+  if residue_mask.sum() == 0:
+    return {
+      'score': np.asarray(0),
+      'num_residues': np.asarray(0, dtype=np.int32),
+      'num_contacts': np.asarray(0, dtype=np.int32),
+    }
+
+  # select only interfacial residues
+  if residue_weights is None:
+    residue_weights = np.ones(logits.shape[0])
+  residue_weights = residue_weights * residue_mask
+
+  # create an inter-chain mask, where only residues relevant to tm scoring are ones.
+  # note that residue pairs of the same chain subject to tm-scoring are zeros.
+  # this force to select the best local frame from residues of other chains.
+  next_id = np.roll(residue_indices, -1)
+  diff = np.abs(next_id - residue_indices)
+  termini = np.where( diff > 1 )[0] + 1
+
+  score = 0
+  num_chains = len(termini)
+  full_length = len(residue_indices)
+  # calculate the tm-score for interface residues of each chain, and sum them.
+  for i in range(num_chains):
+    k = termini[i]
+    if i == 0: k_ = 0
+    else: k_ = termini[i-1]
+    inter_chain_mask = np.zeros((full_length, full_length))
+    inter_chain_mask[k_:k,:] = 1
+    inter_chain_mask[:,k_:k] = 1
+    inter_chain_mask[k_:k,k_:k] = 0
+
+    chain_mask = np.zeros(full_length, dtype=int)
+    chain_mask[k_:k] = 1
+
+    sc = predicted_tm_score( logits, breaks, residue_weights,is_probs=is_probs,
+        chain_mask=chain_mask, inter_chain_mask=inter_chain_mask )
+    #print(f'sc = {sc:.3f}')
+    score += sc
+
+  # return the  interaction score and other interface data
+  return { 'score': score,
+    'num_residues': np.asarray( residue_mask.sum() ),
+    'num_contacts': np.asarray( contact_mask.sum() ) }
