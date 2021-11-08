41860412987f03d5fd878d77e0f7ad86f730b78c
borkd <borkd@users.noreply.github.com>
2021-11-08 14:38:43 -0500

    Update run_relaxation.sh

--- example/run_relaxation.sh
+++ example/run_relaxation.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+# You need to provide DATA_DIR and load_alphafold
+DATA_DIR=$HOME/data/af2ppi/git
 if [[ -r load_alphafold ]]; then
   . load_alphafold  ## set up proper AlphaFold conda environment.
 fi
 
 ### input targets
-target_lst_file=/storage/home/hcoda1/5/gmu3/data/af2ppi/git/example/test.lst
-fea_dir=/storage/home/hcoda1/5/gmu3/data/af2ppi/git/example/af_fea
-out_dir=/storage/home/hcoda1/5/gmu3/data/af2ppi/git/example/af_mod_test
+target_lst_file="$DATA_DIR/example/test.lst"
+fea_dir="$DATA_DIR/example/af_fea"
+out_dir="$DATA_DIR/example/af_mod_test"
 
 echo "Info: input feature directory is $fea_dir"
 echo "Info: result output directory is $out_dir"
 
-af_dir=/storage/home/hcoda1/5/gmu3/data/af2ppi/git//src
-cd $af_dir  || { echo 'Error: enter $af_dir failed' ; exit 1 ; }
+af_dir="$DATA_DIR/src"
+cd "$af_dir" || { echo 'Error: enter $af_dir failed' ; exit 1 ; }
 
 python -u $af_dir/run_alphafold_stage2b.py \
-  --target_lst_path=$target_lst_file \
-  --output_dir=$out_dir \
-  --feature_dir=$fea_dir \
+  --target_lst_path="$target_lst_file" \
+  --output_dir="$out_dir" \
+  --feature_dir="$fea_dir"
