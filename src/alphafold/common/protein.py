edcca4ce7de908ab5c1bac0c29a0b5f16214bf5a
FreshAirTonight <mugao00@gmail.com>
2021-11-08 22:57:47 -0500

    Update protein.py

--- src/alphafold/common/protein.py
+++ src/alphafold/common/protein.py
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Modified for complex structure prediction
+# Mu Gao and Davi Nakajima An
+#
 
 """Protein data type."""
 import io
